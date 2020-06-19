27d4344c0a804b92bd599fe61cd5b1d0d53d9177
Das Karamjit <dkaramji@my.centennialcollege.ca>
2020-06-19 12:12:37 -0700

    added Person class

---
+++ COMP123-M2020-LAB4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_M2020_LAB4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person karamjit = new Person("Karamjit", age: 19);
+
+            karamjit.SaysHello();
+
+            Console.ReadLine();
+        }
+    }
+}
